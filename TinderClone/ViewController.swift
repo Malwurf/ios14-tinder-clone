e6c5374985e5f4c321e4e1bb6683c55f5d7212da
Przemyslaw Koroscik <przemyslaw.koroscik@gmail.com>
2021-01-04 07:56:47 +0100

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Przemyslaw Koroscik on 04/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
